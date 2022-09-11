c1c5b55a41bdbde2eb3d25cc2700f5061a2db446
Trung Huynh <trunghuynh.cse@gmail.com>
2022-09-11 11:51:50 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use crossbeam::channel::*;
+use std::thread;
+use std::time::{Instant};
+
+fn main() {
+    let buf_size = 1024;
+    let producer_msg_no = 1_000_000;
+    let (s, r) = bounded(buf_size);
+    let s2 = s.clone();
+
+    let start_time = Instant::now();
+    // Producer 1
+    let t1 = thread::spawn(move || {
+        let mut i = 0;
+        while i < producer_msg_no {
+            i += 1;
+            s.send(1).unwrap();
+        }
+    });
+
+    // Producer 2
+    let t2 = thread::spawn(move || {
+        let mut i = 0;
+        while i < producer_msg_no {
+            i += 1;
+            s2.send(1).unwrap();
+        }
+    });
+    
+
+    //Consumer
+    let mut sum = 0;
+    for msg in r {
+        let tmp = msg;
+        sum = sum + tmp;
+    }
+
+    t1.join();
+    t2.join();
+
+    let d = Instant::now().duration_since(start_time);
+    let delta = d.as_millis();
+    println!("Sum: {}, processed  time: {}", sum, delta);
+}
